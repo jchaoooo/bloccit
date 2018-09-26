a1cd92d4ac49722839b31111019cb90531f4f82b
Jessica Chao <jchao@SJ-D5510-H84BQF.brg.com>
2018-09-25 18:44:07 -0700

    6-topic-resource-pt2-checkpoint mentor help

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -4,8 +4,8 @@ var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(__filename);
-var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.json')["test"];
+var env       = process.env.NODE_ENV || "development";
+var config    = require(__dirname + '/../config/config.json')["development"];
 var db        = {};
 
 if (config.use_env_variable) {
